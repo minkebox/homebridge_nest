9fb33e6c01e30ca7bcf22160eeea979bc5eefcff
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2019-09-08 16:11:52 -0700

    Initial commit

---
+++ root/app/configure.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+
+ISSUE_TOKEN=$(echo "${ISSUE_TOKEN}" | sed -e 's/[\/&]/\\&/g')
+COOKIES=$(echo "${COOKIES}" | sed -e 's/[\/&]/\\&/g')
+
+cat /app/config.json | sed \
+  -e "s/{{BRIDGE_USERNAME}}/${BRIDGE_USERNAME}/g" \
+  -e "s/{{BRIDGE_PIN}}/${BRIDGE_PIN}/g" \
+  -e "s/{{ISSUE_TOKEN}}/${ISSUE_TOKEN}/g" \
+  -e "s/{{COOKIES}}/${COOKIES}/g" \
+  -e "s/{{API_KEY}}/${API_KEY}/g" \
+  > /app/homebridge/config.json
