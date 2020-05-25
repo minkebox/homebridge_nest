fb34a99e33ff07a9c88edf033d90d44159dcd540
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2020-05-25 16:59:21 -0700

    qrcodes

--- root/app/configure.sh
+++ root/app/configure.sh
@@ -6,6 +6,7 @@ COOKIES=$(echo "${COOKIES}" | sed -e 's/[\/&]/\\&/g')
 cat /app/config.json | sed \
   -e "s/{{BRIDGE_USERNAME}}/${BRIDGE_USERNAME}/g" \
   -e "s/{{BRIDGE_PIN}}/${BRIDGE_PIN}/g" \
+  -e "s/{{BRIDGE_SETUPID}}/${BRIDGE_SETUPID}/g" \
   -e "s/{{ISSUE_TOKEN}}/${ISSUE_TOKEN}/g" \
   -e "s/{{COOKIES}}/${COOKIES}/g" \
   -e "s/{{API_KEY}}/${API_KEY}/g" \
